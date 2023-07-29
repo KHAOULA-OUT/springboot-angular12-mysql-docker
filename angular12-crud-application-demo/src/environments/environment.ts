3ebc49d2f59841a456aeb05d0765cbd389038021
KHAOULA-OUT <85744101+KHAOULA-OUT@users.noreply.github.com>
2023-07-29 16:22:38 +0100

    Update environment.ts

--- angular12-crud-application-demo/src/environments/environment.ts
+++ angular12-crud-application-demo/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl: 'http://192.168.64.4:30163'
+  production: false
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
